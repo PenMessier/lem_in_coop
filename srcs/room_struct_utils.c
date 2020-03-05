39ac8329bff20d7279c2fc60348763e888135ac8
PenMessier <enrudenko13@gmail.com>
2020-03-05 08:15:12 +0300

    Add level

--- srcs/room_struct_utils.c
+++ srcs/room_struct_utils.c
@@ -6,7 +6,7 @@
 /*   By: Elena <Elena@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 12:14:32 by frenna            #+#    #+#             */
-/*   Updated: 2020/02/27 15:07:34 by Elena            ###   ########.fr       */
+/*   Updated: 2020/03/03 19:45:09 by Elena            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,13 @@ t_room		*create_new_room(int x, int y, char *name, int li)
 	new->x = x;
 	new->y = y;
 	new->stat = 0;
+	new->level = -1;
 	new->ant = 0;
 	new->next = NULL;
 	return (new);
 }
 
-int			add_room(t_room *new, t_meta *map, int *nl)
+int				add_room(t_room *new, t_meta *map, int *nl)
 {
 	t_room	*curr;
 
@@ -41,6 +42,7 @@ int			add_room(t_room *new, t_meta *map, int *nl)
 		map->start = *nl == 0 ? new : map->start;
 		map->end = *nl == 1 ? new : map->end;
 		new->stat = *nl == 0 ? 2 : 3;
+		new->level = *nl == 0 ? 0 : new->level;
 	}
 	*nl = 3;
 	if (!map->rooms)
@@ -66,14 +68,34 @@ t_room		*find_room(t_room *rooms, char *name)
 			return (rooms);
 		rooms = rooms->next;
 	}
-	return NULL;
+	return (NULL);
+}
+
+void			assign_level(t_meta *map, int level)
+{
+	t_link	*cur_link;
+
+	cur_link = map->links;
+	while (cur_link)
+	{
+		if (cur_link->start->level == level && cur_link->end->level == -1)
+			cur_link->end->level = level + 1;
+		if (cur_link->end->level == level && cur_link->start->level == -1)
+		{
+			cur_link->start->level = level + 1;
+			swap_rooms(cur_link);
+		}
+		cur_link = cur_link->next;
+	}
+	if (map->end->level == -1)
+		assign_level(map, level + 1);
 }
 
 int		count_index_rooms(t_room *rooms)
 {
 	t_room	*curr;
 	int			i;
-	
+
 	curr = rooms;
 	i = 0;
 	while (curr)
